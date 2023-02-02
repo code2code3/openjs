ca93a7c35eb9172495f1c90195749a0deb2661fb
code2code3 <77616124+code2code3@users.noreply.github.com>
2023-02-02 20:11:14 +0100

    Update ugly10.js

--- ugly10.js
+++ ugly10.js
@@ -624,7 +624,6 @@ function doSomething() {
        document.querySelector('.tags').insertAdjacentElement('beforebegin', ytEmbed);
    }
 
-   setTimeout(() => {
     const ytId = document.querySelector('#yt-video-embed').getAttribute('yt-src');
        const srcdoc = `<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href='https://www.youtube-nocookie.com/embed/${ytId}?autoplay=1'><img src='https://img.youtube.com/vi/${ytId}/default.jpg'><span>▶</span></a>`
 
@@ -638,7 +637,6 @@ function doSomething() {
        iframe.setAttribute('frameborder', '0');
        iframe.setAttribute('allowfullscreen', '');
     document.querySelector('#yt-video-embed').appendChild(iframe);
-   },3500)
       
   }
     
