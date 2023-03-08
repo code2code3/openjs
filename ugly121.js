ead8a00e157e0a3be1a3c2edd4caca0858d59881
code2code3 <77616124+code2code3@users.noreply.github.com>
2023-03-08 17:02:50 +0100

    Update ugly121.js

--- ugly121.js
+++ ugly121.js
@@ -324,21 +324,21 @@
        } else {
            const ps = document.querySelectorAll('.the-content p');
            const midContent = Math.round(ps.length/2);
-           ps[midContent].insertAdjacentHTML('beforebegin', `<ins class="adsbygoogle"
-                style="display:block; text-align:center;"
-                data-ad-layout="in-article"
-                data-ad-format="fluid"
-                data-ad-client="ca-pub-7737139499802016"
-                data-ad-slot="3052253026"></ins>`);
-                
-           //    ps[ps.length-1].insertAdjacentHTML('beforebegin',                 `
-           //    <ins class="adsbygoogle"
+           // ps[midContent].insertAdjacentHTML('beforebegin', `<ins class="adsbygoogle"
            //     style="display:block; text-align:center;"
            //     data-ad-layout="in-article"
            //     data-ad-format="fluid"
            //     data-ad-client="ca-pub-7737139499802016"
-           //     data-ad-slot="5058974421"></ins>
-           //    `);
+           //     data-ad-slot="3052253026"></ins>`);
+                
+               ps[ps.length-1].insertAdjacentHTML('beforebegin',                 `
+               <ins class="adsbygoogle"
+                style="display:block; text-align:center;"
+                data-ad-layout="in-article"
+                data-ad-format="fluid"
+                data-ad-client="ca-pub-7737139499802016"
+                data-ad-slot="5058974421"></ins>
+               `);
                
            let pushScript = document.createElement('script');
            pushScript.innerHTML = "(adsbygoogle = window.adsbygoogle || []).push({});";
